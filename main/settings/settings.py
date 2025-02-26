97f4e9e70cfe606cef249204a6c96f88b4d96d53
Lukáš Klíma <Lukas.klm94@gmail.com>
2025-02-26 13:46:21 +0100

    update static

--- main/settings/settings.py
+++ main/settings/settings.py
@@ -134,17 +134,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static-collected")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static-collected"),
+    os.path.join(BASE_DIR, "static"),
 ]
+COMPRESS_ROOT = BASE_DIR / "static"
 
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
